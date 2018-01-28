b48468de8944f08fc06b38da8e7e46e6b92cb40b
xavier dupré <xavier.dupre@gmail.com>
2018-01-28 01:47:58 +0100

    extend unit test coverage

---
+++ _unittests/ut_module/test_init.py
@@ -0,0 +1,64 @@
+"""
+@brief      test log(time=0s)
+"""
+
+import sys
+import os
+import unittest
+
+
+try:
+    import src
+except ImportError:
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..")))
+    if path not in sys.path:
+        sys.path.append(path)
+    import src
+
+try:
+    import pyquickhelper as skip_
+except ImportError:
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..",
+                "..",
+                "pyquickhelper",
+                "src",)))
+    if path not in sys.path:
+        sys.path.append(path)
+    import pyquickhelper as skip_
+
+
+from pyquickhelper.loghelper import fLOG
+from src.papierstat import check, _setup_hook
+
+
+class TestInit(unittest.TestCase):
+
+    def test_check(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        check()
+
+    def test__setup_hook(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        _setup_hook()
+
+
+if __name__ == "__main__":
+    unittest.main()
