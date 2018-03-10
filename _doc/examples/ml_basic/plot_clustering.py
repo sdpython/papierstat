3678b9b2898df4fa43448c4874da0f9b9a7c979c
xavier dupré <xavier.dupre@gmail.com>
2018-03-10 13:45:44 +0100

    fix issue with matplotlib 2.2.0

--- _doc/examples/ml_basic/plot_clustering.py
+++ _doc/examples/ml_basic/plot_clustering.py
@@ -75,7 +75,10 @@ n_clusters = centers.shape[0]
 ################################
 # On dessine.
 
-import matplotlib.cm as cm
+try:
+    from matplotlib.cm import spectral as color_map
+except ImportError:
+    from matplotlib.cm import summer as color_map
 import numpy
 
 fig, (ax1, ax2) = plt.subplots(1, 2)
@@ -92,7 +95,7 @@ for i in range(n_clusters):
     size_cluster_i = ith_cluster_silhouette_values.shape[0]
     y_upper = y_lower + size_cluster_i
 
-    color = cm.spectral(float(i) / n_clusters)
+    color = color_map(float(i) / n_clusters)
     ax1.fill_betweenx(numpy.arange(y_lower, y_upper),
                       0, ith_cluster_silhouette_values,
                       facecolor=color, edgecolor=color, alpha=0.7)
@@ -109,7 +112,7 @@ ax1.axvline(x=silhouette_avg, color="red", linestyle="--")
 ax1.set_yticks([])
 ax1.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
 
-colors = cm.spectral(cluster_labels.astype(float) / n_clusters)
+colors = color_map(cluster_labels.astype(float) / n_clusters)
 ax2.scatter(X[:, 0], X[:, 1], marker='.', s=30,
             lw=0, alpha=0.7, c=colors, edgecolor='k')
 ax2.scatter(centers[:, 0], centers[:, 1], marker='o',
