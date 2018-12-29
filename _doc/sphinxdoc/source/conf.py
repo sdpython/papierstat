7811cd05c5b36be0cf23436dc2aa79b112df30dc
xavier dupré <xavier.dupre@gmail.com>
2018-12-29 11:33:57 +0100

    update doc style

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -27,9 +27,23 @@ if False:
     html_sidebars['*'] = html_sidebars['**']
     html_sidebars[''] = html_sidebars['*']
 
-html_theme_options = {
-    'touch_icon': '_static/project_ico.ico',
-}
+if html_theme == "bootstrap":
+    html_theme_options = {
+        'navbar_title': ".",
+        'navbar_site_name': "Site",
+        'navbar_links': [
+            ("XD", "http://www.xavierdupre.fr", True),
+        ],
+        'navbar_sidebarrel': True,
+        'navbar_pagenav': True,
+        'navbar_pagenav_name': "Page",
+        'bootswatch_theme': "readable",
+        # united = weird colors, sandstone=green, simplex=red, paper=trop bleu
+        # lumen: OK
+        # to try, yeti, flatly, paper
+        'bootstrap_version': "3",
+        'source_link_position': "footer",
+    }
 
 html_context = {
     'css_files': get_default_stylesheet() + ['_static/my-styles.css', '_static/gallery.css'],
