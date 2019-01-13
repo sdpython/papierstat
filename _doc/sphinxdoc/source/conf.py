cd2db2e0575cc108de46a939f37d0df4eaee831b
xavier dupré <xavier.dupre@gmail.com>
2019-01-13 16:08:34 +0100

    documentation

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -11,7 +11,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0])))
 local_template = os.path.join(os.path.abspath(
     os.path.dirname(__file__)), "phdoc_templates")
 
-set_sphinx_variables(__file__, "papierstat", "Xavier Dupré", 2018,
+set_sphinx_variables(__file__, "papierstat", "Xavier Dupré", 2019,
                      # "guzzle_sphinx_theme", guzzle_sphinx_theme.html_theme_path(),
                      "bootstrap", sphinx_bootstrap_theme.get_html_theme_path(),
                      locals(), extlinks=dict(
