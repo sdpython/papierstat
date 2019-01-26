2a48ff5249ab0c987b2b6559327b246aa6dbb9c0
xavier dupré <xavier.dupre@gmail.com>
2019-01-26 17:23:14 +0100

    extend notebook coverage

---
+++ _unittests/ut_documentation/test_nb_digression_kmeans.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+@brief      test log(time=12s)
+"""
+
+import sys
+import os
+import unittest
+from pyquickhelper.loghelper import fLOG
+from pyquickhelper.pycode import add_missing_development_version
+from pyquickhelper.ipythonhelper import test_notebook_execution_coverage
+
+
+try:
+    import src
+except ImportError:
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..")))
+    if path not in sys.path:
+        sys.path.append(path)
+    import src
+
+import src.papierstat
+
+
+class TestNotebookDigressionKMeans(unittest.TestCase):
+
+    def setUp(self):
+        add_missing_development_version(["jyquickhelper"], __file__, hide=True)
+
+    def test_notebook_digression_kmeans(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        self.assertTrue(src.papierstat is not None)
+        folder = os.path.join(os.path.dirname(__file__),
+                              "..", "..", "_doc", "notebooks", "digressions")
+        test_notebook_execution_coverage(
+            __file__, "kmeans", folder, 'papierstat', copy_files=[], fLOG=fLOG,
+            filter_name=lambda n: "" in n)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ _unittests/ut_documentation/test_nb_encours_linreg.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+@brief      test log(time=10s)
+"""
+
+import sys
+import os
+import unittest
+from pyquickhelper.loghelper import fLOG
+from pyquickhelper.pycode import add_missing_development_version
+from pyquickhelper.ipythonhelper import test_notebook_execution_coverage
+
+
+try:
+    import src
+except ImportError:
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..")))
+    if path not in sys.path:
+        sys.path.append(path)
+    import src
+
+import src.papierstat
+
+
+class TestNotebookEnCoursWines2(unittest.TestCase):
+
+    def setUp(self):
+        add_missing_development_version(["jyquickhelper"], __file__, hide=True)
+
+    def test_notebook_encours_wines(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        self.assertTrue(src.papierstat is not None)
+        folder = os.path.join(os.path.dirname(__file__),
+                              "..", "..", "_doc", "notebooks", "encours")
+        test_notebook_execution_coverage(
+            __file__, "linreg", folder, 'papierstat', copy_files=[], fLOG=fLOG,
+            filter_name=lambda n: "2019-01-25_linreg" in n)
+
+
+if __name__ == "__main__":
+    unittest.main()
