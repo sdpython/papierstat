d26c69d4d0ee654310c7657eb2378821f1eac0ac
xavier dupré <xavier.dupre@gmail.com>
2022-12-10 00:50:21 +0100

    split unit test file

---
+++ _unittests/ut_documentation/test_nb_ts_covid1.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+@brief      test log(time=16s)
+"""
+import os
+import unittest
+from pyquickhelper.loghelper import fLOG
+from pyquickhelper.pycode import add_missing_development_version
+from pyquickhelper.ipythonhelper import test_notebook_execution_coverage
+import papierstat
+
+
+class TestNotebookTsCovid1(unittest.TestCase):
+
+    def setUp(self):
+        add_missing_development_version(["jyquickhelper"], __file__, hide=True)
+
+    def test_notebook_ts_covid(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        self.assertTrue(papierstat is not None)
+        folder = os.path.join(os.path.dirname(__file__),
+                              "..", "..", "_doc", "notebooks", "timeseries")
+        test_notebook_execution_coverage(
+            __file__, "ts_covid", folder, 'papierstat', copy_files=[], fLOG=fLOG)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- _unittests/ut_documentation/test_nb_ts_covid2.py
+++ _unittests/ut_documentation/test_nb_ts_covid2.py
@@ -10,22 +10,10 @@ from pyquickhelper.ipythonhelper import test_notebook_execution_coverage
 import papierstat
 
 
-class TestNotebookTsCovid(unittest.TestCase):
+class TestNotebookTsCovid2(unittest.TestCase):
 
     def setUp(self):
         add_missing_development_version(["jyquickhelper"], __file__, hide=True)
-
-    def test_notebook_ts_covid(self):
-        fLOG(
-            __file__,
-            self._testMethodName,
-            OutputPrint=__name__ == "__main__")
-
-        self.assertTrue(papierstat is not None)
-        folder = os.path.join(os.path.dirname(__file__),
-                              "..", "..", "_doc", "notebooks", "timeseries")
-        test_notebook_execution_coverage(
-            __file__, "ts_covid", folder, 'papierstat', copy_files=[], fLOG=fLOG)
 
     def test_notebook_ts_pred(self):
         fLOG(
